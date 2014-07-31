e7fa03910e3e7336b040afafda30a5ae77990c4d
unknown <a9luu@ucsd.edu>
2014-07-31 01:38:39 -0700

    Took out alert.

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -10,7 +10,6 @@ function getClickHandler() {
 
     // The srcUrl property is only available for image elements.
     var url = 'info.html#' + info.srcUrl;
-      alert(url);
 
     // Create a new window to the info page.
     //chrome.windows.create({ url: url, width: 520, height: 660 });
